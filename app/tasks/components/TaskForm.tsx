bd7e01ea7491a7a808e7aac5cc902249bd195f40
Adeboye Emmanuel <emmanueladeboye2017@gmail.com>
2025-10-01 21:52:44 +0100

    feat: task form work in progress

---
+++ app/tasks/components/TaskForm.tsx
@@ -0,0 +1,66 @@
+import { Circle, MoreVertical, Star } from "lucide-react";
+import { useState } from "react";
+
+export default function TaskForm() {
+  const [isFocused, setIsFocused] = useState(false);
+  const [isHovered, setIsHovered] = useState(false);
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    console.log(e.target);
+  };
+
+  return (
+    <form
+      onSubmit={handleSubmit}
+      className={`flex flex-col gap-2 ${isFocused ? 'bg-focus' : 'hover:bg-focus'} w-full h-fit transition-all duration-300 ease-in-out px-4 py-2`}
+      onFocus={() => setIsFocused(true)}
+      onBlur={() => setIsFocused(false)}
+      onMouseOver={() => setIsHovered(true)}
+      onMouseOut={() => setIsHovered(false)}
+    >
+      <div className="flex gap-6">
+        <Circle size={20} />
+        <textarea
+          className="placeholder:text-md text-md placeholder:text-black placeholder:font-light font-normal w-full focus:outline-none resize-none"
+          placeholder="Title"
+          id="title"
+          name="title"
+          rows={1}
+          style={{
+            ['field-sizing' as any]: 'content',
+            minHeight: '1.5rem',
+            maxHeight: '6rem',
+          }}
+        />
+        <div className="flex items-center gap-2 h-full">
+          {(isFocused || isHovered) && (
+            <div className="animate-in fade-in-0 zoom-in-95 duration-200">
+              <MoreVertical size={18} />
+            </div>
+          )}
+          {(isHovered && !isFocused) && (
+            <div className="animate-in fade-in-0 zoom-in-95 duration-200">
+              <Star size={18} />
+            </div>
+          )}
+        </div>
+      </div>
+
+      <div className="flex gap-2 pl-11">
+        <textarea
+          className="placeholder:text-xs text-xs placeholder:text-black placeholder:font-light font-light w-full focus:outline-none resize-none"
+          placeholder="Details"
+          id="details"
+          name="details"
+          rows={1}
+          style={{
+            ['field-sizing' as any]: 'content',
+            minHeight: '1.5rem',
+            maxHeight: '6rem',
+          }}
+        ></textarea>
+      </div>
+    </form>
+  );
+}
--- app/tasks/components/Taskboard.tsx
+++ app/tasks/components/Taskboard.tsx
@@ -1,6 +1,7 @@
 import { Circle, ListPlus, MoreVertical } from 'lucide-react';
 import Image from 'next/image';
 import { TaskList } from '../../../lib/types/task-list.type';
+import TaskForm from './TaskForm';
 
 interface TaskboardProps {
   taskList: TaskList;
@@ -8,18 +9,19 @@ interface TaskboardProps {
 
 export default function Taskboard({ taskList }: TaskboardProps) {
   return (
-    <div className="flex flex-col gap-4 bg-white p-4 rounded-2xl w-full h-fit break-inside-avoid mb-8 shadow-xs/5 hover:shadow-sm/35 transition-all duration-300 ease-in-out">
-      <div className="flex flex-row items-center justify-between">
+    <div className="flex flex-col gap-4 bg-white py-4 rounded-2xl w-full h-fit break-inside-avoid mb-8 shadow-xs/5 hover:shadow-sm/35 transition-all duration-300 ease-in-out">
+      <div className="flex flex-row items-center justify-between px-4">
         <h2 className="text-lg">{taskList.title}</h2>
         <MoreVertical size={18} />
       </div>
 
-      <div className="flex items-center gap-6 text-primary">
+      <div className="flex items-center gap-6 text-primary px-4">
         <ListPlus />
         <div>Add a task</div>
       </div>
+      <TaskForm />
 
-      <div className="flex flex-col gap-2">
+      <div className="flex flex-col gap-2 px-4">
         {taskList.tasks.length === 0 && (
           <div className="flex flex-col items-center justify-center w-4/5 lg:w-1/2 m-auto text-center gap-4">
             <Image
